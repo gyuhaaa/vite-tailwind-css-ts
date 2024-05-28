22c9063be6bb44f621e848352ce1bd0263f7536d
gyuhaaa <vkvkvk2892@gmail.com>
2024-05-28 17:18:51 +0900

    Count

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import Box from "./components/Box";
 
 const App: FC = () => {
+  const [count, setCount] = useState<number>(0);
+
   return (
-    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center gap-20">
-      <Box name="바나나 박스" bgColor="bg-yellow-500" isRounded={false} />
-      <Box name="딸기 박스" bgColor="bg-red-500" isRounded={true} />
-      <Box name="메론 박스" bgColor="bg-green-500" isRounded={false} />
-      <Box name="오렌지 박스" bgColor="bg-orange-500" />
-      <Box name="포도 박스" bgColor="bg-purple-500" />
+    <div className="bg-red-100 min-h-screen flex justify-center items-center gap-8">
+      <button onClick={() => setCount(count - 1)}>-</button>
+      {count}
+      <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   );
 };
